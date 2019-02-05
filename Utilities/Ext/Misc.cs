b4b35b16604e0e9d710a0ab76f1056ba9b13e76e
Hadi Fooladi Talari <hadi.f7@gmail.com>
2019-02-05 13:43:06 -0500

    v1.1.3

---
+++ Utilities/Ext/Misc.cs
@@ -0,0 +1,8 @@
+﻿namespace HaFT.Utilities
+{
+	public static class MiscExt
+	{
+		public static bool isTrue(this bool? B) => B ?? false;
+		public static bool isFalse(this bool? B) => !(B ?? true);
+	}
+}
