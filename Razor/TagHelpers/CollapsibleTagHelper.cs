3de1ce74c71a9cdf44bc49693e44af2865f331e7
Hadi Fooladi Talari <hadi.f7@gmail.com>
2025-02-27 19:56:19 -0500

    Razor (v1.1.2)
    - FilterTagHelper
    - CollapsibleTagHelper

---
+++ Razor/TagHelpers/CollapsibleTagHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace HaFT.Utilities.Razor.TagHelpers;
+
+public class CollapsibleTagHelper : TagHelper
+{
+	public string? Title { get; set; }
+	public bool IsCollapsed { get; set; }
+
+	public override void Process(TagHelperContext context, TagHelperOutput output)
+	{
+		output.TagName = "div";
+		output.Attributes.SetAttribute("class", "card mb-2 row");
+
+		var id = $"Collapsible{Guid.NewGuid():N}";
+
+		output.PreContent.SetHtmlContent(
+$"""
+<div class="card-header">
+	<div class="row">
+		<div class="col d-flex align-items-center h6 m-0">{Title}</div>
+		<div class="col text-end">
+			<button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#{id}">▼</button>
+		</div>
+	</div>
+</div>
+
+<div class="card-body collapse{(IsCollapsed ? "" : " show")}" id="{id}">
+""");
+
+		output.PostContent.SetHtmlContent("</div>");
+	}
+}
---
+++ Razor/TagHelpers/FilterTagHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace HaFT.Utilities.Razor.TagHelpers;
+
+[HtmlTargetElement("input", Attributes = "filter-name", TagStructure = TagStructure.WithoutEndTag)]
+[HtmlTargetElement("select", Attributes = "filter-name", TagStructure = TagStructure.NormalOrSelfClosing)]
+public class FilterTagHelper : TagHelper
+{
+	public string FilterName { get; set; } = null!;
+
+	public string FilterColClass { get; set; } = "col";
+
+	public override void Process(TagHelperContext context, TagHelperOutput output)
+	{
+		var cls = output.TagName == "input" ? "form-control" : "form-select";
+
+		var attributes = output.Attributes;
+
+		attributes.RemoveAll("filter-*");
+		attributes.SetAttribute("class", cls);
+
+		output.PreElement.SetHtmlContent(
+$"""
+<div class='{FilterColClass}'>
+	<div class='input-group'>
+		<span class='input-group-text align-middle'><pre style='margin:0'>{FilterName}:</pre></span>
+""");
+
+		output.PostElement.SetHtmlContent("</div></div>");
+	}
+}
