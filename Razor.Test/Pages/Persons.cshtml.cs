dde2bc203690fbd98b9fff5a3befee0c9999a141
Hadi Fooladi Talari <hadi.f7@gmail.com>
2025-02-27 20:02:42 -0500

    Razor.Test (v1.0.3) [Ability to filter persons]

--- Razor.Test/Pages/Persons.cshtml.cs
+++ Razor.Test/Pages/Persons.cshtml.cs
@@ -11,8 +11,7 @@ using Column = Models.Table.Column;
 
 public class PersonsModel : TablePageModel<Person>
 {
-	static readonly Database _db;
-
+	#region Static
 	static PersonsModel()
 	{
 		var options = new DbContextOptionsBuilder<Database>()
@@ -36,14 +35,38 @@ public class PersonsModel : TablePageModel<Person>
 		static Person p(string fn, string ln) => new() { FirstName = fn, LastName = ln };
 	}
 
+	static readonly Database _db;
+
+	static readonly IReadOnlyList<Column> s_columns =
+	[
+		Column.Center("#"),
+		Column.Left("First Name"),
+		Column.Left("Last Name")
+	];
+	#endregion
+
 	public PersonsModel()
 	{
 		RowsPerPage = 10;
 	}
 
+	[BindProperty]
+	public FilterType? Filter { get; set; }
+
 	protected override IQueryable<Person> Query => _db.Persons;
 	protected override void ApplySort(ref IQueryable<Person> query) { }
-	protected override void ApplyFilters(ref IQueryable<Person> query, out IEnumerable<string>? filterTexts) { filterTexts = null; }
+
+	protected override void ApplyFilters(ref IQueryable<Person> query, out IEnumerable<string>? filterTexts)
+	{
+		if (Filter == null)
+		{
+			filterTexts = null;
+			return;
+		}
+
+		query = Filter.Apply(query, out var list);
+		filterTexts = list;
+	}
 
 	protected override Table GetTable(IQueryable<Person> query, int startingNum)
 	{
@@ -56,10 +79,20 @@ public class PersonsModel : TablePageModel<Person>
 		}
 	}
 
-	static readonly IReadOnlyList<Column> s_columns =
-	[
-		Column.Center("#"),
-		Column.Left("First Name"),
-		Column.Left("Last Name")
-	];
+	public class FilterType
+	{
+		public string? FirstName { get; set; }
+		public string? LastName { get; set; }
+
+		public IQueryable<Person> Apply(IQueryable<Person> query, out LinkedList<string>? filterTexts)
+		{
+			var builder = new QueryBuilder<Person>(query);
+
+			builder.CheckContains("FirstName", FirstName);
+			builder.CheckContains("LastName", LastName);
+
+			filterTexts = builder.Texts;
+			return builder.Query;
+		}
+	}
 }
---
+++ Razor.Test/QueryBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace HaFT.Utilities.Razor.Test;
+
+class QueryBuilder<T>
+{
+	public QueryBuilder(IQueryable<T> query) { Query = query; }
+
+	public IQueryable<T> Query { get; private set; }
+	public LinkedList<string>? Texts { get; private set; }
+
+	public void CheckEquiality(string propertyName, string? value)
+	{
+		if (string.IsNullOrWhiteSpace(value)) return;
+
+		AddFilterText(propertyName, value);
+		Query = Query.Where(p => EF.Property<string>(p, propertyName) == value);
+	}
+
+	public void CheckContains(string propertyName, string? value)
+	{
+		if (string.IsNullOrWhiteSpace(value)) return;
+
+		AddFilterText(propertyName, value);
+		Query = Query.Where(p => EF.Property<string>(p, propertyName).Contains(value));
+	}
+
+	public void AddFilterText(string variable, object value)
+	{
+		Texts ??= [];
+		Texts.AddLast($"{variable} = {value}");
+	}
+}
