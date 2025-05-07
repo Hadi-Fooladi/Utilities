b80d24078158307e42327090861688035e3de4fc
Hadi Fooladi Talari <hadi.f7@gmail.com>
2025-05-07 00:14:16 -0400

    New Tests

--- Tests/Razor/StyleBuilderTests.cs
+++ Tests/Razor/StyleBuilderTests.cs
@@ -24,4 +24,33 @@ public class StyleBuilderTests
 		Assert.Equal(12, builder["font-size"]);
 		Assert.Equal("color:red;font-size:12", builder.ToString());
 	}
+
+	[Fact]
+	public void Remove()
+	{
+		var builder = new StyleBuilder()
+			.Add("color", "red")
+			.Add("font-size", 12)
+			.Remove("color");
+
+		Assert.Null(builder["color"]);
+		Assert.Equal(12, builder["font-size"]);
+		Assert.Equal("font-size:12", builder.ToString());
+	}
+
+	[Fact]
+	public void Base()
+	{
+		var builder = new StyleBuilder
+			{
+				Base = new StyleBuilder()
+					.Add("color", "red")
+					.Add("font-size", 12)
+			}
+			.Add("font-size", 14);
+		
+		Assert.Equal("red", builder["color"]);
+		Assert.Equal(14, builder["font-size"]);
+		Assert.Equal("color:red;font-size:14", builder.ToString());
+	}
 }
