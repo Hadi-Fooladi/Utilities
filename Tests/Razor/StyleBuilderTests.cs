720c0f8033691e369ba530019a512041748835a1
Hadi Fooladi Talari <hadi.f7@gmail.com>
2025-05-06 23:50:49 -0400

    New Project: Tests/Razor (xUnit Test Project)

---
+++ Tests/Razor/StyleBuilderTests.cs
@@ -0,0 +1,15 @@
+﻿namespace HaFT.Utilities.Razor.Tests;
+
+using Models.HTML;
+
+public class StyleBuilderTests
+{
+	[Fact]
+	public void Empty()
+	{
+		var builder = new StyleBuilder();
+
+		Assert.Empty(builder.Names);
+		Assert.Equal(string.Empty, builder.ToString());
+	}
+}
