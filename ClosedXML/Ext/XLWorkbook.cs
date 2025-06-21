8939eb44e92bd02e72eb546b08ca843e85fe2d88
Hadi Fooladi Talari <hadi.f7@gmail.com>
2025-06-21 11:08:47 -0400

    ClosedXML (v1.1.2)
    New extension method: XLWorkbook.GetBytes

---
+++ ClosedXML/Ext/XLWorkbook.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+using ClosedXML.Excel;
+
+namespace HaFT.Utilities.ClosedXML;
+
+public static class XLWorkbookExt
+{
+	public static byte[] GetBytes(this XLWorkbook book)
+	{
+		using var ms = new MemoryStream();
+		book.SaveAs(ms);
+		return ms.ToArray();
+	}
+}
